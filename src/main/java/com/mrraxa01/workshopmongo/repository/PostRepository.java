9a8ae08e95c8157d4dbb858df5283a5959a19a4d
mrraxa01 <mrraxa01@gmail.com>
2020-10-21 19:01:55 -0300

    Criando Entity Post com user aninhado

---
+++ src/main/java/com/mrraxa01/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.mrraxa01.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mrraxa01.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
