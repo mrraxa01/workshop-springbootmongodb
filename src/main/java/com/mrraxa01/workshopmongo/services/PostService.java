20c68e8d2ffd72fb4f3bbe6c219220ac155e7483
mrraxa01 <mrraxa01@gmail.com>
2020-10-21 21:36:56 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/mrraxa01/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.mrraxa01.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mrraxa01.workshopmongo.domain.Post;
+import com.mrraxa01.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+		
+	public Post findById(String id) {
+	
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+	}
+	
+		
+	
+}
